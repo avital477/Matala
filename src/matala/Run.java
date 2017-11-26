b48b3dccd866a86cf75f870e06b5162dfc02e817
avital477 <avital477@gmail.com>
2017-11-26 23:15:15 +0200

    commit

--- src/matala/Run.java
+++ src/matala/Run.java
@@ -14,13 +14,21 @@ public class Run {
 	openFile	openFileObject = new openFile();
 	writeFile	writeFileObject = new writeFile();
 	RowFunctions rowFunctionObject = new RowFunctions();
+	Filter FilterObject = new Filter();
 
+/**
+ * The heart of the program
+ * @param location
+ * @param destination
+ */
 	public void Program(String location, String destination){
-		destination = destination+"final.csv";
+		String root = destination;
+		destination = destination+"mergeCSV.csv";
 		matrix = openFolderObject.openFolders(location, destination);
 		rowFunctionObject.sortBySignal(matrix);
 		rowFunctionObject.uniteDates(matrix);
 		rowFunctionObject.headLine(matrix);
 		writeFileObject.writeFiles(destination, matrix);
+		FilterObject.fileChoose(destination, root);
 	}
 }
